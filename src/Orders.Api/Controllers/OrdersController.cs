e57e45c33ac33f2d53da730c4a6082fca331b9f7
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-12-18 17:48:59 -0300

    Added POST endpoint /api/orders using use cases

---
+++ src/Orders.Api/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Application.UseCases.CreateOrder;
+
+namespace Orders.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly CreateOrderHandler _handler;
+
+        public OrdersController(CreateOrderHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateOrderRequest request, CancellationToken ct)
+        {
+            var id = await _handler.HandleAsync(request, ct);
+            return Accepted(new { id });
+        }
+    }
+}
---
+++ src/Orders.Application/UseCases/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orders.Domain.Entities;
+
+namespace Orders.Application.UseCases.CreateOrder
+{
+    public class CreateOrderHandler
+    {
+        public Task<Guid> HandleAsync(CreateOrderRequest request, CancellationToken ct = default)
+        {
+            var order = new Order(Guid.NewGuid(), request.CustomerName, request.Amount, request.OrderDate);
+            return Task.FromResult(order.Id);
+        }
+    }
+}
